8a1918ae9632572dcf35ecc5e9d38caf90bbefaa
Simon Lynch <simon.lynch@hashicorp.com>
2025-05-15 15:40:46 +1000

    Add Terraform resources for TFE projects configuration

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@
 data "tfe_organization" "this" {
   name = var.tfe_organization_name
 }
+
+
+# resource to create each tfe_project using map
+
+resource "tfe_project" "this" {
+  for_each = var.tfe_projects
+
+  name        = each.value.name
+  description = each.value.description
+  organization = data.tfe_organization.this.id
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "tfe_organization_name" {
   description = "TFE organization name"
   type        = string
 }
+
+variable "tfe_projects" {
+  description = "Map of TFE projects to create"
+  type        = map(object({
+    name        = string
+    description = string
+  }))
+  default     = {
+    "project1" = {
+      name        = "Demo Project - IaaS"
+      description = "Description for Project 1"
+    }
+    "project2" = {
+      name        = "Demo Project 2"
+      description = "Description for Project 2"
+    }
+  }
+}
