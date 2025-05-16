87cf6f851ea8515b27b97443a0c626125baabbef
Simon Lynch <simon.lynch@hashicorp.com>
2025-05-16 16:39:06 +1000

    Add resource for TFE variable sets and update project variable set configuration

--- main.tf
+++ main.tf
@@ -13,3 +13,22 @@ resource "tfe_project" "this" {
   description = each.value.description
   organization = data.tfe_organization.this.name
 }
+
+
+# create a project variable set for each project
+resource "tfe_variable_set" "this" {
+  for_each = var.tfe_projects
+
+  name        = "${each.value.name}-var-set"
+  description = "${each.value.description} varset"
+  organization = data.tfe_organization.this.name
+}
+
+
+
+resource "tfe_project_variable_set" "test" {
+  for_each = var.tfe_projects
+  
+  variable_set_id = tfe_variable_set.this[each.key].id
+  project_id      = tfe_project.this[each.key].id
+}
